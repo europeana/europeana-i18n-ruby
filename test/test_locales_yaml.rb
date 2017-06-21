9e2543e8b58171ff3bda67bb50dda21a0f7578c8
Richard Doe <richard.doe@rwdit.net>
2017-06-21 12:45:05 +0100

    Test each locale YAML file has one key for its language

--- test/test_locales_yaml.rb
+++ test/test_locales_yaml.rb
@@ -8,10 +8,21 @@ class TestLocalesYAML < Minitest::Test
     FileList.new("config/locales/#{set}/??.yml").each do |file_path|
       locale = File.basename(file_path, '.yml')
 
-      define_method :"test_#{set}_locale_#{locale}" do
-        translations = YAML.load_file(file_path)
-        assert_instance_of(Hash, translations)
+      define_method :"test_#{set}_locale_#{locale}_yaml_parses" do
+        assert_instance_of(Hash, load_translations(file_path))
+      end
+
+      define_method :"test_#{set}_locale_#{locale}_key" do
+        translations = load_translations(file_path)
+        assert_equal(1, translations.keys.size)
+        assert_equal(locale, translations.keys.first)
       end
     end
+  end
+
+  private
+
+  def load_translations(file_path)
+    YAML.load_file(file_path)
   end
 end
